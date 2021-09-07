6473c3ed95c38aefc7d5344be8c1e93af5e8199d
andrey <andre547481@gmail.com>
2021-09-07 14:37:13 +0300

    Версия после рефакторинга
    	изменено:      src/ts_checkmodel.cpp

--- src/ts_checkmodel.cpp
+++ src/ts_checkmodel.cpp
@@ -9,7 +9,7 @@
 #include <regex>
 
 void Deallocator([[maybe_unused]]void* data, [[maybe_unused]]size_t length, [[maybe_unused]]void* arg) {
-        std::cout << "Deallocator called\n";
+        // std::cout << "Deallocator called\n";
         // free(data);
         // *reinterpret_cast<bool*>(arg) = true;
 }
@@ -19,14 +19,9 @@ class TF_container {
 public:  
   TF_Buffer* RunOpts = nullptr;
   TF_Status* status = nullptr;
-  // std::unique_ptr<TF_Status, decltype(&TF_DeleteStatus)> status;
   TF_Graph* graph = nullptr;
   TF_SessionOptions* session_opts = nullptr;
   TF_Session* session = nullptr;
-
-  // std::unique_ptr<TF_container> tf_cont(new TF_container);
-  // using unique_file_t = std::unique_ptr<std::FILE, decltype(&close_file)>;
-  // auto song = make_unique<Song>(L"Mr. Children", L"Namonaki Uta"
 
   TF_container()/* : status(TF_NewStatus(), TF_DeleteStatus)*/ {
     allocate();
@@ -99,6 +94,137 @@ using feat_arr_t = std::vector<features_data_t>;
 
 feat_arr_t read_file (const std::string &testDataFile);
 
+// ######################
+// Set up graph inputs
+// ######################
+int SetGraphInput(
+  std::unique_ptr<TF_container> &tf_cont,
+  features_data_t               &features_data,
+
+  std::vector<TF_Output>        &inputs, 
+  std::vector<TF_Tensor*>       &input_values
+  ) 
+{  
+  // Create vectors to store graph input operations and input tensors
+  // std::vector<TF_Output> inputs;
+  // std::vector<TF_Tensor*> input_values;
+
+  // Create variables to store the size of the input variables
+  int64_t in_dims[] = {1, 28, 28, 1};
+
+  // Set input dimensions - this should match the dimensionality of the input in
+  // the loaded graph, in this case it's three dimensional.
+  const int num_bytes_in = 28 * 28 * sizeof(float);
+
+  // Pass the graph and a string name of your input operation
+  // (make sure the operation name is correct)
+  TF_Operation* input_op = TF_GraphOperationByName(tf_cont->graph, "serving_default_input");
+  if (input_op == nullptr) {
+    std::cout << "Operation 'serving_default_input' not found in graph" << std::endl;
+    return 1;
+  }
+  // std::cout << "Input op info: " << TF_OperationNumOutputs(input_op) << "\n";
+
+  TF_Output input_opout = {input_op, 0};
+  inputs.push_back(input_opout);
+
+  // Create the input tensor using the dimension (in_dims) and size (num_bytes_in)
+  // variables created earlier
+  TF_Tensor* input = TF_NewTensor(TF_FLOAT, in_dims, 4, features_data.features.data(), num_bytes_in, &Deallocator, 0);
+  if (input == nullptr) {
+      std::cerr << "Error: TF_NewTensor" << std::endl;
+      return 1;
+  }
+  // Optionally, you can check that your input_op and input tensors are correct
+  // by using some of the functions provided by the C API.
+  // std::cout << "Input data info: " << TF_Dim(input, 0) << "\n";
+
+  input_values.push_back(input);
+
+  return 0;
+}
+  
+// ######################
+// Set up graph outputs (similar to setting up graph inputs)
+// ######################
+int SetGraphOutput(
+  std::unique_ptr<TF_container> &tf_cont,
+
+  std::vector<TF_Output>        &outputs,
+  std::vector<TF_Tensor*>       &output_values
+  ) 
+{  
+  // Create variables to store the size of the output variables
+  int64_t out_dims[] = {1, 10};
+
+  // Set input dimensions - this should match the dimensionality of the input in
+  // the loaded graph, in this case it's three dimensional.
+  const int num_bytes_out = 10 * sizeof(float);
+
+  // Create vector to store graph output operations
+  // std::vector<TF_Output> outputs;
+
+  TF_Operation* output_op = TF_GraphOperationByName(tf_cont->graph, "StatefulPartitionedCall");
+  if (output_op == nullptr) {
+    std::cout << "Operation 'StatefulPartitionedCall' not found in graph" << std::endl;
+    return 1;
+  }
+  TF_Output output_opout = {output_op, 0};
+  outputs.push_back(output_opout);
+
+  // Create TF_Tensor* vector
+  // std::vector<TF_Tensor*> output_values(outputs.size(), nullptr);
+  output_values.resize(outputs.size(), nullptr);
+
+  // Similar to creating the input tensor, however here we don't yet have the
+  // output values, so we use TF_AllocateTensor()
+  TF_Tensor* output_value = TF_AllocateTensor(TF_FLOAT, out_dims, 2, num_bytes_out);
+  output_values.push_back(output_value);
+
+  // As with inputs, check the values for the output operation and output tensor
+  // std::cout << "Output: " << TF_OperationName(output_op) << "\n";
+  // std::cout << "Output info: " << TF_Dim(output_value, 0) << "\n";
+  // std::cout << "Output info: " << TF_Dim(output_value, 1) << "\n";
+
+  return 0;
+}
+
+int CalculatePrediction(
+  std::unique_ptr<TF_container> &tf_cont,
+
+  std::vector<TF_Output>        &inputs,
+  std::vector<TF_Tensor*>       &input_values, 
+  std::vector<TF_Output>        &outputs,
+  std::vector<TF_Tensor*>       &output_values,
+  int                           &predicted
+  )
+{
+  // Call TF_SessionRun
+  TF_SessionRun(tf_cont->session, nullptr,
+                &inputs[0], &input_values[0], inputs.size(),
+                &outputs[0], &output_values[0], outputs.size(),
+                nullptr, 0, nullptr, tf_cont->status);
+
+  if (TF_GetCode(tf_cont->status) != TF_OK) {
+          std::cout << "ERROR: Unable to run session " << TF_Message(tf_cont->status) << std::endl;
+          return 1;
+  }
+
+  probas_t probas;
+  float* out_vals = static_cast<float*>(TF_TensorData(output_values[0]));
+  for (int i = 0; i < 10; ++i)
+  {
+      probas.push_back(*out_vals);
+      // std::cout << "Output values[" << i << "] info: " << *out_vals << "\n";
+      out_vals++;
+  }
+
+  auto argmax = std::max_element(probas.begin(), probas.end());
+  predicted = std::distance(probas.begin(), argmax);
+
+  return 0;
+}
+
 int main(int argc, char* argv[]) {
   if (argc < 3) {
       std::cout << "Usage: " << argv[0] <<  " modelpath test_data_file" << std::endl;
@@ -112,24 +238,8 @@ int main(int argc, char* argv[]) {
 
   auto feat_arr = read_file (testDataFile);
 
-  // DEBUG >>>
-  // auto feat_data = *feat_arr.begin();
-
-  // std::cout << feat_data.product_type << std::endl;
-
-  // for (const auto val : feat_data.features) {
-  //   std::cout << val << " ";
-  // }
-  // std::cout << std::endl;
-  // <<< DEBUG
-
   std::unique_ptr<TF_container> tf_cont(new TF_container);
 
-  // TF_Graph* graph = TF_NewGraph();
-  // TF_Status* status = TF_NewStatus();
-  // TF_SessionOptions* session_opts = TF_NewSessionOptions();
-  // TF_Buffer* RunOpts = NULL;
-  
   // Load saved TensorFlow session
   std::cout << "Start load TensorFlow saved model from " << export_dir << std::endl;
 
@@ -153,113 +263,49 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Successfully load session" << std::endl;
 
- // Create variables to store the size of the input and output variables
-  const int num_bytes_in = 28 * 28 * sizeof(float);
-  const int num_bytes_out = 10 * sizeof(float);
-
-  // Set input dimensions - this should match the dimensionality of the input in
-  // the loaded graph, in this case it's three dimensional.
-  int64_t in_dims[] = {1, 28, 28, 1};
-  int64_t out_dims[] = {1, 10};
-
-  size_t pos = 0;
-  TF_Operation* oper;
-  while ((oper = TF_GraphNextOperation(tf_cont->graph, &pos)) != nullptr) {
-      std::cout << "Input: " << TF_OperationName(oper) << "\n";
-  }
+  // size_t pos = 0;
+  // TF_Operation* oper;
+  // while ((oper = TF_GraphNextOperation(tf_cont->graph, &pos)) != nullptr) {
+  //     std::cout << "Input: " << TF_OperationName(oper) << "\n";
+  // }
 
   int count_positive = 0;
   int count_negative = 0;
 
   // Check on test data
   for (auto features_data : feat_arr) {
+   
+    std::vector<TF_Output>        inputs;
+    std::vector<TF_Tensor*>       input_values;
 
-    // ######################
-    // Set up graph inputs
-    // ######################
+    int result = SetGraphInput(tf_cont, features_data, inputs, input_values);
 
-    // Create vectors to store graph input operations and input tensors
-    std::vector<TF_Output> inputs;
-    std::vector<TF_Tensor*> input_values;
-
-    // Pass the graph and a string name of your input operation
-    // (make sure the operation name is correct)
-    TF_Operation* input_op = TF_GraphOperationByName(tf_cont->graph, "serving_default_input");
-    if (input_op == nullptr) {
-      std::cout << "Operation 'serving_default_input' not found in graph" << std::endl;
-      return 1;
-    }
-    // std::cout << "Input op info: " << TF_OperationNumOutputs(input_op) << "\n";
-
-    TF_Output input_opout = {input_op, 0};
-    inputs.push_back(input_opout);
-
-    // Create the input tensor using the dimension (in_dims) and size (num_bytes_in)
-    // variables created earlier
-    TF_Tensor* input = TF_NewTensor(TF_FLOAT, in_dims, 4, features_data.features.data(), num_bytes_in, &Deallocator, 0);
-    if (input == nullptr) {
-        std::cerr << "Error: TF_NewTensor" << std::endl;
-        return 1;
-    }
-    // Optionally, you can check that your input_op and input tensors are correct
-    // by using some of the functions provided by the C API.
-    // std::cout << "Input data info: " << TF_Dim(input, 0) << "\n";
-
-    input_values.push_back(input);
-
-
-    // ######################
-    // Set up graph outputs (similar to setting up graph inputs)
-    // ######################
-
-    // Create vector to store graph output operations
-    std::vector<TF_Output> outputs;
-    TF_Operation* output_op = TF_GraphOperationByName(tf_cont->graph, "StatefulPartitionedCall");
-    if (output_op == nullptr) {
-      std::cout << "Operation 'StatefulPartitionedCall' not found in graph" << std::endl;
-      return 1;
-    }
-    TF_Output output_opout = {output_op, 0};
-    outputs.push_back(output_opout);
-
-    // Create TF_Tensor* vector
-    std::vector<TF_Tensor*> output_values(outputs.size(), nullptr);
-
-    // Similar to creating the input tensor, however here we don't yet have the
-    // output values, so we use TF_AllocateTensor()
-    TF_Tensor* output_value = TF_AllocateTensor(TF_FLOAT, out_dims, 2, num_bytes_out);
-    output_values.push_back(output_value);
-
-    // As with inputs, check the values for the output operation and output tensor
-    std::cout << "Output: " << TF_OperationName(output_op) << "\n";
-    std::cout << "Output info: " << TF_Dim(output_value, 0) << "\n";
-    std::cout << "Output info: " << TF_Dim(output_value, 1) << "\n";
-
-    // Call TF_SessionRun
-    TF_SessionRun(tf_cont->session, nullptr,
-                  &inputs[0], &input_values[0], inputs.size(),
-                  &outputs[0], &output_values[0], outputs.size(),
-                  nullptr, 0, nullptr, tf_cont->status);
-
-    if (TF_GetCode(tf_cont->status) != TF_OK) {
-            std::cout << "ERROR: Unable to run session " << TF_Message(tf_cont->status) << std::endl;
-            return 1;
+    if (result != 0) {
+      std::cout << "ERROR: SetGraphInput " << std::endl;
+      return result;
     }
 
-    probas_t probas;
-    float* out_vals = static_cast<float*>(TF_TensorData(output_values[0]));
-    for (int i = 0; i < 10; ++i)
-    {
-        probas.push_back(*out_vals);
-        // std::cout << "Output values[" << i << "] info: " << *out_vals << "\n";
-        out_vals++;
+    std::vector<TF_Output>        outputs;
+    std::vector<TF_Tensor*>       output_values;
+
+    result = SetGraphOutput(tf_cont, outputs, output_values);
+  
+    if (result != 0) {
+      std::cout << "ERROR: SetGraphOutput " << std::endl;
+      return result;
     }
 
-    auto argmax = std::max_element(probas.begin(), probas.end());
-    int predicted = std::distance(probas.begin(), argmax);
+    int    predicted;
 
-    std::cout << "Predicted values : " << predicted << "\n";
-    std::cout << "Estimated values : " << features_data.product_type << "\n";
+    result = CalculatePrediction(tf_cont, inputs, input_values, outputs, output_values, predicted);
+
+    if (result != 0) {
+      std::cout << "ERROR: CalculatePrediction " << std::endl;
+      return result;
+    }
+
+    // std::cout << "Predicted values : " << predicted << "\n";
+    // std::cout << "Estimated values : " << features_data.product_type << "\n";
 
     if (predicted == features_data.product_type) {
       count_positive++;
@@ -284,7 +330,6 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-
 feat_arr_t read_file (const std::string &testDataFile)
 {
   feat_arr_t    feat_arr;
@@ -292,7 +337,6 @@ feat_arr_t read_file (const std::string &testDataFile)
 
   const std::regex comma(",");
   
-  // std::ifstream test_data{"test.csv"/*file.c_str()*/};
   std::ifstream test_data{testDataFile.c_str()};
   if (!test_data.is_open() ) {
     std::string err = "Input data file " + testDataFile + " not found";
@@ -300,26 +344,6 @@ feat_arr_t read_file (const std::string &testDataFile)
   }
 
   std::cout << "Start load test.csv" << std::endl;
-/*
-  while ( getline (test_data,line) ) {
-
-        std::vector<std::string> row{ std::sregex_token_iterator(line.begin(),line.end(),comma,-1), 
-                std::sregex_token_iterator() };
-
-        features_data_t    features_data;
-        features_data.features.reserve(row.size());
-
-        // first byte - product type
-        auto it = row.begin();
-        features_data.product_type = stof(*it++);
-
-        // Divide each bytes by 255
-        std::transform(it, row.end(), std::back_inserter(features_data.features),
-                [](std::string val) { return stof(val) / 255;});
-
-        feat_arr.push_back(features_data);
-  }
-*/
 
   while ( getline (test_data,line) ) {
 
